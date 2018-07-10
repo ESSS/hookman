91060828fb18dc9a86cfb89a528e49b41a7b893d
William Jamir Silva <williamjamir@gmail.com>
2018-07-10 10:52:45 -0300

    Internal refactor

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,23 +1,8 @@
-from pathlib import Path
 from shutil import copy2
 
 import pytest
 
 from hookman.hooks import HookMan, HooksSpecs
-
-
-def test_find_plugin(datadir, libs_path):
-    def friction_factor(v1: 'int', v2: 'int') -> 'int':
-        """
-        Docs for Friction Factor
-        """
-
-    plugin_specs = HooksSpecs(project_name='acme', version='1', pyd_name='_test_hooks', hooks=[friction_factor])
-
-    HookMan(specs=plugin_specs, plugin_dirs=[datadir])
-
-    with pytest.raises(FileNotFoundError, match="The given path doesn't have a .yaml file"):
-        HookMan(specs=plugin_specs, plugin_dirs=[Path('NON_EXISTING_PATH')])
 
 
 def test_hook_specs_without_arguments():
@@ -28,7 +13,8 @@ def test_hook_specs_without_arguments():
 
     # A hook must have parameters
     with pytest.raises(TypeError, match="It's not possible to create a hook without argument"):
-        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme', hooks=[method_without_arguments])
+        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme',
+            hooks=[method_without_arguments])
 
 
 def test_hook_specs_with_missing_type_on_argument():
@@ -39,7 +25,8 @@ def test_hook_specs_with_missing_type_on_argument():
 
     # A arguments of the hook must inform the type
     with pytest.raises(TypeError, match="All hooks arguments must have the type informed"):
-        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme', hooks=[method_with_missing_type_on_argument])
+        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme',
+            hooks=[method_with_missing_type_on_argument])
 
 
 def test_hook_specs_without_docs_arguments():
@@ -48,7 +35,8 @@ def test_hook_specs_without_docs_arguments():
 
 
     with pytest.raises(TypeError, match="All hooks must have documentation"):
-        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme', hooks=[method_with_docs_missing])
+        specs = HooksSpecs(project_name='acme', version='1', pyd_name='_acme',
+            hooks=[method_with_docs_missing])
 
 
 def test_get_hook_caller(datadir, libs_path):
@@ -72,5 +60,23 @@ def test_get_hook_caller(datadir, libs_path):
     hm = HookMan(specs=plugin_specs.specs, plugin_dirs=[datadir])
     hook_caller = hm.get_hook_caller()
     friction_factor = hook_caller.friction_factor()
+    env_temperature = hook_caller.env_temperature()
+    assert friction_factor is not None
+    assert env_temperature is None
     assert friction_factor(1, 2) == 3
 
+
+def test_get_hook_caller_without_plugin(datadir, libs_path):
+    # Load the hook_specs.py (inside the test folder) into plugin_specs
+    hook_specs = datadir / 'hook_specs.py'
+    import importlib
+    spec = importlib.util.spec_from_file_location('hook_specs', hook_specs)
+    plugin_specs = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(plugin_specs)
+
+    hm = HookMan(specs=plugin_specs.specs, plugin_dirs=[datadir / 'some_non_existing_folder'])
+    hook_caller = hm.get_hook_caller()
+    friction_factor = hook_caller.friction_factor()
+    env_temperature = hook_caller.env_temperature()
+    assert friction_factor is None
+    assert env_temperature is None

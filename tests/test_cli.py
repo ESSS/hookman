3bb2d97300f31e71973bf749c1fbf8128bad148b
Bruno Oliveira <bruno@esss.com.br>
2019-01-21 15:33:18 -0200

    Show the command-line usage of the code generator

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,19 +1,19 @@
 from click.testing import CliRunner
 
-from hookman import cli
+from hookman import __main__
 
 
 def test_command_line_interface(datadir):
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(__main__.main)
     assert result.exit_code == 2
     assert result.output == 'Usage: main [OPTIONS] SPECS_PATH\nTry "main --help" for help.' \
                             '\n\nError: Missing argument "SPECS_PATH".\n'
 
     hook_spec_file = str(datadir / 'hook_specs.py')
-    result = runner.invoke(cli.main, [hook_spec_file, '--dst-path', datadir])
+    result = runner.invoke(__main__.main, [hook_spec_file, '--dst-path', datadir])
     assert result.exit_code == 0
 
-    help_result = runner.invoke(cli.main, ['--help'])
+    help_result = runner.invoke(__main__.main, ['--help'])
     assert help_result.exit_code == 0

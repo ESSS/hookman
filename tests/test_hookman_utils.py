0f6bb0c8d25e9760127649184fe4f5ab8a2d9b2c
Gustavo G. Ribeiro <gribeiro@esss.co>
2019-04-29 16:17:34 -0300

    Add test for `load_shared_lib`

--- tests/test_hookman_utils.py
+++ tests/test_hookman_utils.py
@@ -8,3 +8,16 @@ def test_find_config_files(datadir):
 
     config_files = find_config_files([datadir / 'non_existing_folder'])
     assert len(config_files) == 0
+
+
+def test_load_shared_lib(simple_plugin_dll):
+    import os
+    import sys
+    from hookman.hookman_utils import load_shared_lib
+
+    if sys.platform.startswith('win'):
+        assert not str(simple_plugin_dll.parent) in os.environ["PATH"]
+        with load_shared_lib(str(simple_plugin_dll)):
+            pass
+
+        assert str(simple_plugin_dll.parent) in os.environ["PATH"]
